7cdd2a3737d492a0292099b9c00c7784ec503525
Zaklina Radenkovic <zradenkovic@hotmail.com>
2022-07-17 14:23:22 +0200

    adding css file and updating App

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { Component } from "react";
-
-import logo from "./logo.svg";
+import CardList from "./components/card-list/card-list";
+import SearchBox from "./components/search-box/search-box";
 import "./App.css";
 
 class App extends Component {
@@ -8,34 +8,82 @@ class App extends Component {
     super();
 
     this.state = {
-      name: "Jackie",
-      company: "ZTM",
+      monsters: [],
+      searchField: "",
     };
   }
 
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) =>
+        // console.log(response)
+        response.json()
+      )
+      .then((users) =>
+        //  console.log(users);
+        this.setState(
+          () => {
+            return { monsters: users };
+          }
+          // () => {
+          //   console.log(this.state);
+          // }
+        )
+      );
+  }
+
+  onSearchChange = (e) => {
+    const searchField = e.target.value.toLocaleLowerCase();
+    this.setState(() => {
+      return { searchField };
+    });
+  };
+
   render() {
+    const { monsters, searchField } = this.state;
+    const { onSearchChange } = this;
+    const filteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField);
+    });
+
+    console.log("render from App");
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Hi, {this.state.name} I work at {this.state.company}
-          </p>
-          <button
-            onClick={() => {
-              this.setState(
-                () => {
-                  return { name: "Bata" };
-                },
-                () => {
-                  console.log(this.state);
-                }
-              );
-            }}
-          >
-            Change name
-          </button>
-        </header>
+        <h1 className="app-title">Monsters Rolodex</h1>
+
+        {/* <input
+          className="search-box"
+          type="search"
+          placeholder="search monsters"
+          // //why we cannot return all monsters once we start deleting the letters?
+          // onChange={(e) => {
+          //   // ex with console.log
+          //   console.log({ startingArr: this.state.monsters });
+          //   //making input values small
+          //   const searchString = e.target.value.toLocaleLowerCase();
+          //   const filteredMonsters = this.state.monsters.filter((monster) => {
+          //     //the same here for monster of searching
+          //     return monster.name.toLocaleLowerCase().includes(searchString);
+          //   });
+          //   this.setState(
+          //     () => {
+          //       return { monsters: filteredMonsters };
+          //     },
+          //     //the answer is because we are filtering filteredMonsters
+          //     () => console.log({ endingArr: filteredMonsters })
+          //   );
+          // }}
+
+          onChange={onSearchChange}
+        /> */}
+
+        <SearchBox
+          onChangeHandler={onSearchChange}
+          placeholder="search monsters"
+          className="monsters-search-box"
+        />
+
+        <CardList monsters={filteredMonsters} />
       </div>
     );
   }
